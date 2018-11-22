73a9a343e865a9899384cfc52d28e0b0319a80ba
iHDeveloper <hamzadevelop@gmail.com>
2018-11-22 22:04:06 +0300

    Let other modules to have access throw our module

--- src/main/java/org/terasology/TutorialWorldGenerator/TutorialWorldGenerator.java
+++ src/main/java/org/terasology/TutorialWorldGenerator/TutorialWorldGenerator.java
@@ -40,6 +40,7 @@ public class TutorialWorldGenerator extends BaseFacetedWorldGenerator {
                 .addProvider(new SeaLevelProvider(0))
                 .addProvider(new MountainsProvider())
                 .addProvider(new HouseProvider())
-                .addRasterizer(new TutorialWorldRasterizer());
+                .addRasterizer(new TutorialWorldRasterizer())
+                .addPlugins();
     }
 }
