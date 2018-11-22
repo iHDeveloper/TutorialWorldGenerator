ba15d8021c51fb1f4eff5675e9c93f810a98176a
iHDeveloper <hamzadevelop@gmail.com>
2018-11-22 18:21:04 +0300

    Add some noises ways

--- src/main/java/org/terasology/TutorialWorldGenerator/SurfaceProvider.java
+++ src/main/java/org/terasology/TutorialWorldGenerator/SurfaceProvider.java
@@ -17,6 +17,10 @@ package org.terasology.TutorialWorldGenerator;
 
 import org.terasology.math.geom.BaseVector2i;
 import org.terasology.math.geom.Rect2i;
+import org.terasology.math.geom.Vector2f;
+import org.terasology.utilities.procedural.Noise;
+import org.terasology.utilities.procedural.SimplexNoise;
+import org.terasology.utilities.procedural.SubSampledNoise;
 import org.terasology.world.generation.Border3D;
 import org.terasology.world.generation.FacetProvider;
 import org.terasology.world.generation.GeneratingRegion;
@@ -26,9 +30,11 @@ import org.terasology.world.generation.facets.SurfaceHeightFacet;
 @Produces(SurfaceHeightFacet.class)
 public class SurfaceProvider implements FacetProvider {
 
+    private Noise surfaceNoise;
+
     @Override
     public void setSeed(long seed) {
-
+        surfaceNoise = new SubSampledNoise(new SimplexNoise(seed), new Vector2f(.01f, .01f), 1);
     }
 
     @Override
@@ -37,7 +43,7 @@ public class SurfaceProvider implements FacetProvider {
         SurfaceHeightFacet facet = new SurfaceHeightFacet(region.getRegion(), border);
         Rect2i processRegion = facet.getWorldRegion();
         for (BaseVector2i position : processRegion.contents()) {
-            facet.setWorld(position, 10f);
+            facet.setWorld(position, surfaceNoise.noise(position.x(), position.y()) * 20);
         }
         region.setRegionFacet(SurfaceHeightFacet.class, facet);
     }
